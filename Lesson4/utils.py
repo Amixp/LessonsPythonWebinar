34b0ea9be6600ce855b3ec123442f0e6d1ff6731
Artem <artemxp@gmail.com>
2022-04-18 00:58:39 +1000

    доработка, скрипт с модулем
    Signed-off-by: Artem <artemxp@gmail.com>

--- Lesson4/utils.py
+++ Lesson4/utils.py
@@ -2,6 +2,9 @@
 # (например, USD, EUR, GBP, ...) и возвращающую курс этой валюты по отношению к рублю.
 # Использовать библиотеку requests. В качестве API можно использовать
 # http://www.cbr.ru/scripts/XML_daily.asp.
+from requests import get
+from decimal import Decimal
+
 
 def currency_rates(cur_code):
     """ Отдает курс по коду cur_code этой валюты по отношению к рублю"""
@@ -10,13 +13,11 @@ def currency_rates(cur_code):
     for i in r.text.split('<CharCode>')[1:]:
         value = Decimal(i.split('Value')[1].strip("<>/").replace(",", "."))
         values.setdefault(i[0:3], value)
-    return values.get(cur_code[1].upper())
+    return values.get(cur_code.upper())
 
 
 if __name__ == '__main__':
     import sys
-    from decimal import Decimal
-    from requests import get
     """ печатает в консоль курс по коду из первого параметра """
-    print(currency_rates(sys.argv))
+    print(currency_rates(sys.argv[1]))
     exit(0)
